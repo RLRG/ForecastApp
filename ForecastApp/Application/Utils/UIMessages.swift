6dab84da1e159fa12543d31019d79bdabdb6bed3
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2018-04-29 00:13:44 +0200

    feat: add mainPresenter with view logic

--- ForecastApp/Application/Utils/UIMessages.swift
+++ ForecastApp/Application/Utils/UIMessages.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+/**
+ UIMessages
+ 
+ It contains the messages being displayed to the user of the app.
+ */
+struct UIMessages {
+    
+    // MARK: - General
+    static let errorGeneralTitle = NSLocalizedString("Error", comment: "")
+    static let errorGeneral = NSLocalizedString("It has not been possible to update the Weather information. Please, check if you have Internet connection and try again.", comment: "")
+}
--- ForecastApp/Presentation/Main/Presenter/MainPresenter.swift
+++ ForecastApp/Presentation/Main/Presenter/MainPresenter.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+import RxSwift
+
+/**
+ MainPresenter
+ 
+ Object responsible for providing the weather forecast to the view, telling the view what to display and when. It contains all the logic of the main screen.
+ 
+ This class behaves as the manager of a states machine of the UIViewController. The possible view states can be found in the class "MainViewState"
+ */
+class MainPresenter {
+    
+    // MARK: - Properties & Initialization
+    
+    /// Rx PublishSubject responsible for sending the events from the presenter to the UIViewController - MainVC
+    private var mainViewState : PublishSubject<MainViewState> = PublishSubject<MainViewState>()
+    /// Rx PublishSubject observable to the one the UIViewController - MainVC is attached to get the all the possible states
+    var mainViewStateObservable : Observable<MainViewState> {
+        return mainViewState.asObservable()
+    }
+    /// The current ViewState of the UIViewController attached - MainVC
+    private var currentViewState : MainViewState
+    
+    /// SearchWeather use case abstraction - Interactor - used to get the weather data
+    private let getWeatherForecastUseCase: SearchWeatherProtocol
+    
+    /// Bag to dispose added disposables [RxSwift]
+    private let disposeBag = DisposeBag()
+    
+    /**
+     Initialization method for the MainPresenter class
+     - Parameter getUserProfileUseCase: The interactor layer to call the getUserProfile functionality
+     - Parameter saveUserProfileUseCase: The interactor layer to call the saveUserProfile functionality
+     - Parameter logoutUseCase: The interactor layer to call the logout functionality
+     */
+    init(getWeatherForecastUseCase: SearchWeatherProtocol) {
+        self.getWeatherForecastUseCase = getWeatherForecastUseCase
+        
+        // Set the default viewState to "loading"
+        currentViewState = MainViewState.loading
+    }
+    
+    // MARK: - Logic
+    
+    /**
+     Called by the UIViewController when is ready to display data, it gets the weatherForecast
+     */
+    func viewIsReady() {
+        getWeatherForecast()
+    }
+    
+    /**
+     Call the SearchWeather use case to get the weather forecast data. If it succeeds, the presenter sets the UIViewController to the "displayWeatherInfo" state. If it does not succeed, the presenter tells the UIViewController that an error must be displayed
+     */
+    func getWeatherForecast() {
+        self.currentViewState = MainViewState.loading
+        self.mainViewState.onNext(self.currentViewState)
+        
+        getWeatherForecastUseCase
+            .getWeatherForecast(withName: nil, withLat: 0.0, withLon: 0.0) // TODO: Set appropriate parameter values
+            .subscribe(
+                onNext: { weatherResult in
+                    self.currentViewState = MainViewState.displayWeatherInfo(weatherResult: weatherResult)
+                    self.mainViewState.onNext(self.currentViewState)
+            },
+                onError: { error in
+                    self.displayError(with: UIMessages.errorGeneralTitle, and: UIMessages.errorGeneral)
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    /**
+     Set the UIViewController to error state by displaying an error alert
+     - Parameter title: the title for the error alert
+     - Parameter message: the message to be displayed inside the error alert
+     */
+    func displayError(with title: String, and message: String) {
+        self.currentViewState = MainViewState.error(title: title, message: message)
+        self.mainViewState.onNext(self.currentViewState)
+    }
+}
